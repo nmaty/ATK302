a988db2a058083e44be22c7644b3ff6b731bf2f8
Nick Maty <nmaty@ilstu.edu>
2019-08-22 11:07:18 -0500

    Sup

--- p5/tree/sketch.js
+++ p5/tree/sketch.js
@@ -1,11 +1,7 @@
 function setup() {
   // put setup code here
-  createCanvas(500, 500);
 }
 
 function draw() {
   // put drawing code here
-rect(150,100,200,300)
-
-  ellipse(mouseX, mouseY, 10, 10) ;
 }
